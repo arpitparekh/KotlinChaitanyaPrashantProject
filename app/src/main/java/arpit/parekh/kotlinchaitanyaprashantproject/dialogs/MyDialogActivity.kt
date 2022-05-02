5f4dcecb7feacb6986220ded841bb86f219b8012
arpitparekh <arpitparekh9@gmail.com>
2022-05-02 16:40:39 +0530

    progress bar spinner and multi choice and single choice

--- app/src/main/java/arpit/parekh/kotlinchaitanyaprashantproject/dialogs/MyDialogActivity.kt
+++ app/src/main/java/arpit/parekh/kotlinchaitanyaprashantproject/dialogs/MyDialogActivity.kt
@@ -1,5 +1,7 @@
 package arpit.parekh.kotlinchaitanyaprashantproject.dialogs
 
+import android.app.Dialog
+import android.app.ProgressDialog
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,18 +10,26 @@ import androidx.appcompat.app.AlertDialog
 import arpit.parekh.kotlinchaitanyaprashantproject.R
 import arpit.parekh.kotlinchaitanyaprashantproject.databinding.ActivityMyDialogBinding
 import arpit.parekh.kotlinchaitanyaprashantproject.databinding.DialogLayoutBinding
+import arpit.parekh.kotlinchaitanyaprashantproject.databinding.ProgressSpinnerBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MyDialogActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMyDialogBinding
     lateinit var dialogBinding: DialogLayoutBinding
+    lateinit var progressSpinnerBinding : ProgressSpinnerBinding
+    lateinit var items : ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyDialogBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        items = ArrayList()
+
+        val hobbies = arrayOf("Cycling","gym","coding","reading")
+        val checks = booleanArrayOf(false,true,true,false)
 
         binding.btnAlert.setOnClickListener {
 
@@ -71,6 +81,70 @@ class MyDialogActivity : AppCompatActivity() {
                 binding.tvSomething.text = data
             })
             builder.show()
+
+        }
+
+        binding.btnMultiChoice.setOnClickListener {
+
+            items.clear()
+
+            items.add(hobbies[1])
+            items.add(hobbies[2])
+
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("Choose Hobbies")
+
+            builder.setMultiChoiceItems(hobbies,booleanArrayOf(false,true,true,false), DialogInterface.OnMultiChoiceClickListener { dialogInterface, i, b ->
+
+                if(b){
+                    items.add(hobbies[i])
+                }else{
+                    items.remove(hobbies[i])
+                }
+            })
+
+            builder.setPositiveButton("Ok", DialogInterface.OnClickListener { dialogInterface, i ->
+
+                Toast.makeText(this,items.toString(),Toast.LENGTH_SHORT).show()
+
+            })
+
+            builder.show()
+
+
+        }
+
+        binding.btinSingleChoiceDialog.setOnClickListener {
+
+            AlertDialog.Builder(this)
+                .setTitle("Choose One")
+                .setSingleChoiceItems(hobbies,2, DialogInterface.OnClickListener { dialogInterface, i ->
+
+                    Toast.makeText(this,hobbies[i],Toast.LENGTH_SHORT).show()
+
+                }).create().show()
+
+
+        }
+
+        binding.btnProgress1.setOnClickListener {
+
+
+            progressSpinnerBinding = ProgressSpinnerBinding.inflate(layoutInflater)
+
+//            val pd = ProgressDialog(this)
+//            pd.setTitle("Downloading")
+//            pd.setMessage("Please Wait")
+//            pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+//            pd.show()
+
+            val dialog = Dialog(this)
+            dialog.setContentView(progressSpinnerBinding.root)
+            dialog.show()
+
+//            dialog.dismiss()
+
 
         }
 
